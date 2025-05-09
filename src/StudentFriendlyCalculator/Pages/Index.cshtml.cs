a5ecd6dabec9015d3bc377379e76858a6e40ff23
Kaevrin <159499822+Kaevrin@users.noreply.github.com>
2025-05-09 12:37:50 -0600

    Demonstration of javascript and C# interaction.

--- src/StudentFriendlyCalculator/Pages/Index.cshtml.cs
+++ src/StudentFriendlyCalculator/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace StudentFriendlyCalculator.Pages;
 
+[IgnoreAntiforgeryToken]
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
@@ -15,5 +16,18 @@ public class IndexModel : PageModel
     public void OnGet()
     {
 
+    }
+
+    public class UserInput
+    {
+        public required string Name { get; set; }
+    }
+
+    
+    public IActionResult OnPostProcessName([FromBody] UserInput input)
+    {
+        Console.WriteLine("Received: " + input.Name); // or use a breakpoint
+        string message = $"Hello, {input.Name}!";
+        return new JsonResult(new { message });
     }
 }
