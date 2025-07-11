ac048cc6b786be547439a964894a62f649db67e4
Yucheol-Son <ldsycson@byui.edu>
2025-07-11 15:09:22 -0600

    add factorial function

--- src/StudentFriendlyCalculator/Pages/Index.cshtml.cs
+++ src/StudentFriendlyCalculator/Pages/Index.cshtml.cs
@@ -30,37 +30,37 @@ public class IndexModel : PageModel
     //This is proving difficult because I cannot find a list which states NCalc's functions anywhere.
     //Another unique problem I have is that words are not always separated by spaces, but may be separated by paranthesis or operators. Using the split function would not do, the deliminators are still critical parts of the string.
     //See Allyn for questions.
-    private string ReformatString(string originalMessage)
-    {
-        originalMessage = originalMessage.ToLower();
-        List<string> strings = new();
-        string parseMe = "";
-        string newMessage = "";
-        foreach (char c in originalMessage)
-        {
-            if (Char.IsLetter(c))
-            {
-                parseMe += c;
-            }
-            else
-            {
-                if (parseMe != "")
-                {
-                    newMessage += capitalizeWord(parseMe);
-                    parseMe = "";
-                }
-                if (parseMe != "x")
-                    newMessage += c;
-            }
-        }
-        //This next line is just in case the final letter in the string was a letter, not a symbol. The above loop would have never caught such a situation. For example, "pi" or "e" would fit this case.
-        if (parseMe != "")
-        {
-            newMessage += capitalizeWord(parseMe);
-        }
-        Console.WriteLine("Parsed \"" + originalMessage + "\", into:\"" + newMessage + "\"");
-        return newMessage;
-    }
+    // private string ReformatString(string originalMessage)
+    // {
+    //     originalMessage = originalMessage.ToLower();
+    //     List<string> strings = new();
+    //     string parseMe = "";
+    //     string newMessage = "";
+    //     foreach (char c in originalMessage)
+    //     {
+    //         if (Char.IsLetter(c))
+    //         {
+    //             parseMe += c;
+    //         }
+    //         else
+    //         {
+    //             if (parseMe != "")
+    //             {
+    //                 newMessage += capitalizeWord(parseMe);
+    //                 parseMe = "";
+    //             }
+    //             if (parseMe != "x")
+    //                 newMessage += c;
+    //         }
+    //     }
+    //     //This next line is just in case the final letter in the string was a letter, not a symbol. The above loop would have never caught such a situation. For example, "pi" or "e" would fit this case.
+    //     if (parseMe != "")
+    //     {
+    //         newMessage += capitalizeWord(parseMe);
+    //     }
+    //     Console.WriteLine("Parsed \"" + originalMessage + "\", into:\"" + newMessage + "\"");
+    //     return newMessage;
+    // }
 
 
     //See Allyn for questions.
@@ -73,6 +73,19 @@ public class IndexModel : PageModel
         return parseMe;
     }
 
+    private double Factorial(double n)
+    {
+        if (n < 0 || n != Math.Floor(n))
+            throw new ArgumentException("Factorial only defined for non-negative integers.");
+
+        double result = 1;
+        for (int i = 2; i <= (int)n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
 
     public IActionResult OnPostCalcSubmission([FromBody] UserInput input)
     {
@@ -83,7 +96,7 @@ public class IndexModel : PageModel
             return new JsonResult(new { error = "Expression cannot be empty." });
         }
 
-        input.Name = ReformatString(input.Name);
+        // input.Name = ReformatString(input.Name);
 
         var expr = new Expression(input.Name);
 
@@ -95,6 +108,51 @@ public class IndexModel : PageModel
             if (name.ToLower() == "e")
                 args.Result = Math.E;
         };
+
+        // Add support for factorial(n)
+        EvaluateParameterHandler parameterHandler = (name, args) =>
+        {
+            if (name.ToLower() == "pi")
+                args.Result = Math.PI;
+            else if (name.ToLower() == "e")
+                args.Result = Math.E;
+        };
+
+        // 공통 EvaluateFunction 핸들러 (재귀 처리 포함)
+        EvaluateFunctionHandler functionHandler = null;
+        functionHandler = (name, args) =>
+        {
+            if (name.ToLower() == "factorial")
+            {
+                if (args.Parameters.Length != 1)
+                    throw new ArgumentException("factorial() requires exactly one argument.");
+
+                object evaluated = args.Parameters[0].Evaluate();
+
+                if (evaluated is int intVal)
+                {
+                    args.Result = Factorial(intVal);
+                }
+                else if (evaluated is long longVal)
+                {
+                    args.Result = Factorial((double)longVal);
+                }
+                else if (evaluated is double doubleVal)
+                {
+                    args.Result = Factorial(doubleVal);
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid argument to factorial()");
+                }
+
+            }
+        };
+
+
+        // 원래 Expression에 등록
+        expr.EvaluateParameter += parameterHandler;
+        expr.EvaluateFunction += functionHandler;
 
         try
         {
