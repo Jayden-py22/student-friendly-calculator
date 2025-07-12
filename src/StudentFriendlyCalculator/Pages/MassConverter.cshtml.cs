01345bf40e1554023e44bac52a728d689cca9bc8
Kaevrin <159499822+Kaevrin@users.noreply.github.com>
2025-07-12 10:28:28 -0600

    Pushing for unification

--- src/StudentFriendlyCalculator/Pages/MassConverter.cshtml.cs
+++ src/StudentFriendlyCalculator/Pages/MassConverter.cshtml.cs
@@ -2,15 +2,75 @@ using System.Net.NetworkInformation;
 using Antlr.Runtime;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using NCalc;
 
 namespace Calculator.Pages
 {
-    public class MassConvertertModel : PageModel
+    public class MassConverterModel : PageModel
     {
-        private readonly ILogger<MassConvertertModel> _logger;
+        private readonly ILogger<MassConverterModel> _logger;
+        private double ConvertUnits(double value, string fromUnit, string toUnit)
+        {
+            if (fromUnit == toUnit)
+                return value;
 
-        public MassConvertertModel(ILogger<MassConvertertModel> logger)
+            // Mass conversions
+            switch (fromUnit)
+            {
+                case "pound":
+                    switch (toUnit)
+                    {
+                        case "gram": return value * 453.592;
+                        case "kilogram": return value * 0.453592;
+                        case "ton": return value * 0.000453592;
+                        case "ounce": return value * 16;
+                    }
+                    break;
+
+                case "gram":
+                    switch (toUnit)
+                    {
+                        case "pound": return value / 453.592;
+                        case "kilogram": return value / 1000;
+                        case "ton": return value / 1_000_000;
+                        case "ounce": return value / 28.3495;
+                    }
+                    break;
+
+                case "kilogram":
+                    switch (toUnit)
+                    {
+                        case "pound": return value / 0.453592;
+                        case "gram": return value * 1000;
+                        case "ton": return value / 1000;
+                        case "ounce": return value * 35.274;
+                    }
+                    break;
+
+                case "ton":
+                    switch (toUnit)
+                    {
+                        case "pound": return value / 0.000453592;
+                        case "gram": return value * 1_000_000;
+                        case "kilogram": return value * 1000;
+                        case "ounce": return value * 35274;
+                    }
+                    break;
+
+                case "ounce":
+                    switch (toUnit)
+                    {
+                        case "pound": return value / 16;
+                        case "gram": return value * 28.3495;
+                        case "kilogram": return value / 35.274;
+                        case "ton": return value / 35274;
+                    }
+                    break;
+            }
+
+            throw new ArgumentException($"Unsupported conversion from '{fromUnit}' to '{toUnit}'.");
+        }
+
+        public MassConverterModel(ILogger<MassConverterModel> logger)
         {
             _logger = logger;
         }
@@ -20,93 +80,22 @@ namespace Calculator.Pages
 
         }
 
-        public class UserInput
+        public class ConversionRequest
         {
-            public required string Name { get; set; }
+            public string FromUnit { get; set; }
+            public string ToUnit { get; set; }
+            public double Value { get; set; }
         }
-
-
-        //The function below is meant to help parse the user input so that it matches the capitalization NCalc is expecting.
-        //This is proving difficult because I cannot find a list which states NCalc's functions anywhere.
-        //Another unique problem I have is that words are not always separated by spaces, but may be separated by paranthesis or operators. Using the split function would not do, the deliminators are still critical parts of the string.
-        //See Allyn for questions.
-        private string ReformatString(string originalMessage)
+        public IActionResult OnPostConverterSubmission([FromBody] ConversionRequest input)
         {
-            originalMessage = originalMessage.ToLower();
-            List<string> strings = new();
-            string parseMe = "";
-            string newMessage = "";
-            foreach (char c in originalMessage)
-            {
-                if (Char.IsLetter(c))
-                {
-                    parseMe += c;
-                }
-                else
-                {
-                    if (parseMe != "")
-                    {
-                        newMessage += capitalizeWord(parseMe);
-                        parseMe = "";
-                    }
-                    if (parseMe != "x")
-                        newMessage += c;
-                }
-            }
-            //This next line is just in case the final letter in the string was a letter, not a symbol. The above loop would have never caught such a situation. For example, "pi" or "e" would fit this case.
-            if (parseMe != "")
-            {
-                newMessage += capitalizeWord(parseMe);
-            }
-            Console.WriteLine("Parsed \"" + originalMessage + "\", into:\"" + newMessage + "\"");
-            return newMessage;
-        }
-
-
-        //See Allyn for questions.
-        //Capitalizes the word if it is not a pi or e
-        private string capitalizeWord(string parseMe)
-        {
-            char firstLetter = parseMe[0];
-            string otherLetters = parseMe.Remove(0, 1);
-            parseMe = firstLetter.ToString().ToUpper() + otherLetters;
-            return parseMe;
-        }
-
-
-        public IActionResult OnPostConverterSubmission([FromBody] UserInput input)
-        {
-            Console.WriteLine("Received: " + input.Name); // The math expression
-
-            if (string.IsNullOrWhiteSpace(input.Name))
-            {
-                return new JsonResult(new { error = "Expression cannot be empty." });
-            }
-
-            input.Name = ReformatString(input.Name);
-
-            var expr = new Expression(input.Name);
-
-            // Add support for constants like pi
-            expr.EvaluateParameter += (name, args) =>
-            {
-                if (name.ToLower() == "pi")
-                    args.Result = Math.PI;
-                if (name.ToLower() == "e")
-                    args.Result = Math.E;
-            };
-
             try
             {
-                var result = expr.Evaluate();
-                Console.WriteLine($"Result: {result}");
+                double result = ConvertUnits(input.Value, input.FromUnit.ToLower(), input.ToUnit.ToLower());
                 return new JsonResult(new { result });
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex);
-                Console.WriteLine("Error: " + ex.Message);
-                return new JsonResult(new { error = "Invalid expression: " + ex.Message });
+                return new JsonResult(new { error = ex.Message });
             }
         }
 
